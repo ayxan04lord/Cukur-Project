d7e78eed9660ff99cafd478b0bf4c6b6cba0f984
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2024-08-25 14:54:16 +0400

    convert to toolkit

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,8 +6,8 @@ import { addItem, addItem2 } from '../../store/card/cardReducer';
 const Card = ({ id, image, title, content }) => {
     const numOfItems = useSelector((state) => state.card);
     const numOfBasket = useSelector((state) => state.card);
+    
     const dispatch = useDispatch();
-    console.log(numOfItems)
     const clickAdd = () => {
         dispatch(addItem(id));
     }
@@ -17,17 +17,16 @@ const Card = ({ id, image, title, content }) => {
     }
     
     return (
-        
         <div className="card">
             <div className="card-content">
                 <img style={{ objectFit: "cover" }} width={270} height={300} src={image} alt={title} />
                 <h3 className="card-title">{title}</h3>
                 <p className="card-text">{content}</p>
                 <button className='like' onClick={clickAdd} type='like'>
-                    {/* To Like {numOfItems} */}
+                    To Like {numOfItems.items[id]}
                 </button>
                 <button className='basket' onClick={clickHandleAdd}>
-                    {/* To Basket {numOfBasket} */}
+                    To Basket {numOfBasket.baskets[id]}
                 </button>
             </div>
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,32 @@
 import './Navbar.css';
 import logo from '../../img/logo.jpg';
 import { useSelector } from 'react-redux';
-import { selectTotalBasketSum } from './NavbarSelector';
-
-
 
 const Navbar = () => {
-
-
-
-    // const totalBasketSum = useSelector(selectTotalBasketSum);
-
+    const getBaskets = useSelector((state) => state.card.baskets);
+    let basketsCalculate = 0;
+    if (Object.keys(getBaskets).length !== 0)
+        basketsCalculate = Object.values(getBaskets).reduce((acc, cur) => (acc += cur));
 
     return (
-        <div className="container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div className="container" style={{ display: "flex",flexDirection: "row", justifyContent: "space-between", alignItems: "center", backgroundColor: "gold", padding: "0px 32px" }}>
+            <div className="nav__logo">
+                <a href="#">
+                    <img width={20} src={logo} alt="logo" />
+                </a>
+            </div>
             <ul >
-                <li><a href="#"><img width={20} src={logo} alt="logo" /></a></li>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Products</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">About</a></li>
-                {/* <li style={{ marginLeft: 'auto' }}><i className='fa fa-shopping-basket'></i>{totalBasketSum}</li> */}
             </ul>
-        </div>
 
+            <div className="nav__baskets">
+                <i className='fa fa-shopping-basket'></i>
+                <span>{basketsCalculate}</span>
+            </div>
+        </div>
     );
 };
 
